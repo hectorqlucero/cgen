272472bc06fd8d8030cd541f45839db922af9183
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2026-06-08 09:08:30 -0700

    setup

--- project.clj
+++ project.clj
@@ -76,7 +76,11 @@
              ;; i18n lint: validate translation keys across source and locale files
              ;;   lein i18n-lint
              ;;   lein i18n-lint src        ; scan a specific directory
-            "i18n-lint" ["run" "-m" "cgen.i18n.lint"]}
+            "i18n-lint" ["run" "-m" "cgen.i18n.lint"]
+             ;; Setup new project
+             ;; lein setup my-project - creates project on same folder - lein setup contactos
+             ;; lein setup /my-path/my-project - creates project on specified path - lein /tmp/contactos
+            "setup" ["run" "-m" "cgen.tools.setup" "--"]}
   :profiles {:uberjar {:aot :all
                        :main cgen.core
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
